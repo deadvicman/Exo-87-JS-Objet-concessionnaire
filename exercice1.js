a1f60d853a5077dd3b4a03827b494f99b66e7c17
victorien <deadvicmangame@gmail.com>
2022-12-06 11:11:31 +0100

    exo 87 plutot simple

--- exercice1.js
+++ exercice1.js
@@ -10,6 +10,42 @@ let voiture = {
     "vitesseMaxi" : "120km/h"
 };
 
+let nom = document.createElement("h1");
+nom.innerText = voiture.Nom;
+document.body.append(nom);
+
+let img = document.createElement("img");
+img.src = voiture.imgUrl;
+document.body.append(img);
+
+let nbRoues = document.createElement("p");
+nbRoues.innerText = voiture.nombresRoues + " Roues";
+document.body.append(nbRoues);
+
+let couleur = document.createElement("p");
+couleur.innerText = 'La voiture est de couleur ' + voiture.Couleur;
+document.body.append(couleur);
+
+let constructeur = document.createElement("p");
+constructeur.innerText = 'Voiture construite par ' + voiture.Contructeur;
+document.body.append(constructeur);
+
+let carburant = document.createElement("p");
+carburant.innerText = 'La voiture utilise le carburant suivant: ' + voiture.Carburant;
+document.body.append(carburant);
+
+let nbPortes = document.createElement("p");
+nbPortes.innerText = 'Nombre de portes: ' + voiture.nombrePortes;
+document.body.append(nbPortes);
+
+let autonomie = document.createElement("p");
+autonomie.innerText = 'Autonomie de ' + voiture.Autonomie;
+document.body.append(autonomie);
+
+let vitesseMax = document.createElement("p");
+vitesseMax.innerText = 'Vitesse de pointe: ' + voiture.vitesseMaxi;
+document.body.append(vitesseMax);
+
 
 
 
